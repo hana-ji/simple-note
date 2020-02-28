8397f21f6d384ca27b9a0ad9c16685d11a41f141
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-02-28 16:37:57 +0900

    state로 데이터 관리

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,7 +6,19 @@ import List from '../List';
 import Note from '../Note';
 
 class App extends React.Component {
-  
+  // state: UI의 상태를 기록하는 데이터
+  state = {
+    // 앱의 노트 정보를 담은 array
+    notes: [
+      {
+        id: '_asdf3das',
+        title: '제목1',
+        contents: '내용1'
+      }
+    ],
+    // List에서 유저가 선택하여 활성화 된 노트의 id를 기록
+    activeId: null,
+  }
   render() {
     return (
       <div className="App">
